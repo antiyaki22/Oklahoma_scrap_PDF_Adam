b946daf21f07b560a7fdd916aaf667eb995e4619
antiyaki22 <kyryloheiko3@gmail.com>
2025-03-17 16:41:13 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -213,7 +213,7 @@ def extract_info_from_json(json_file_path):
             text = re.sub(r'(\bowned\s*by)([A-Z])', r'\1 \2', text)  
             text = clean_text(text)
 
-            address_pattern = r'(\d+\s[\w\s.,#-]+?),\s*([A-Za-z\s]+),\s*([A-Z]{2})\s*(\d{5}(-\d{4})?)?'
+            address_pattern = r'(?:[\w\s&.,-]+?)?\s*(\d+\s[\w\s.,#-]+?),\s*([A-Za-z\s]+),\s*([A-Z]{2})\s*(\d{5}(-\d{4})?)?'
             match = re.search(address_pattern, text)
             if match:
                 return match.group(1), match.group(2), match.group(3), match.group(4) if match.group(4) else None
@@ -222,7 +222,7 @@ def extract_info_from_json(json_file_path):
             if owner_address_match:
                 return owner_address_match.group(1), owner_address_match.group(2), owner_address_match.group(3), owner_address_match.group(4) if owner_address_match.group(4) else None
 
-            flexible_pattern = r'(\d+\s[\w\s.,#/-]+?(Way|St|Ave|Blvd|Rd|Dr|Lane|Ct|Pl|Terrace|Drive|Pkwy))\s*,?\s*([A-Za-z\s]+?)\s*,?\s*([A-Z]{2})\s*(\d{5}(-\d{4})?)?'
+            flexible_pattern = r'(?:[\w\s&.,-]+?)?\s*(\d+\s[\w\s.,#/-]+?(Way|St|Ave|Blvd|Rd|Dr|Lane|Ct|Pl|Terrace|Drive|Pkwy))\s*,?\s*([A-Za-z\s]+?)\s*,?\s*([A-Z]{2})\s*(\d{5}(-\d{4})?)?'
             matches = re.findall(flexible_pattern, text)
             if matches:
                 for match in matches:
